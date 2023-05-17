8434fae2631d4ff144325e4df48e4e8ee317d221
rhrubelr <rhrubel246@gmail.com>
2023-05-17 23:33:40 +0600

    added Main and Home routs

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+    return (
+        <div>
+            <h4>this is main page</h4>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h4>this is home</h4>
+        </div>
+    );
+};
+
+export default Home;
