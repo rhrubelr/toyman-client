f75beedba7fecb09bb0e40b78a2333e52362d842
rhrubelr <rhrubel246@gmail.com>
2023-05-18 14:17:10 +0600

    added registration form

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -1,8 +1,40 @@
+import { Link } from "react-router-dom";
 
 const Registration = () => {
     return (
-        <div>
-            <h4>is this registration pages</h4>
+        <div className='mb-10 container w-50 mx-auto bg-cyan-950 rounded-lg py-10'>
+            <h2 className=' w-80 py-2 mt-2 mx-auto text-purple-100 font-bold text-lg rounded-lg bg-purple-900 text-center'>Please Register Your Account </h2>
+
+            <form  className="form-control w-full max-w-xs mx-auto">
+                <label className="label">
+                    <span className="label-text font-bold">What is your name ?</span>
+
+                </label>
+                <input type="text" name='name' placeholder="name" required className="input input-bordered w-full max-w-xs" />
+
+                <label className="label">
+                    <span className="label-text font-bold">What is your image URL ?</span>
+
+                </label>
+                <input type="text" name='photo' placeholder="photo url" required className="input input-bordered w-full max-w-xs" />
+
+                <label className="label">
+                    <span className="label-text font-bold">What is your Email ?</span>
+
+                </label>
+                <input type="email" name='email' placeholder="email" required className="input input-bordered w-full max-w-xs" />
+
+                <label className="label">
+                    <span className="label-text font-bold">What is your Password?</span>
+
+                </label>
+                <input type="password" name='password' placeholder="password" required className="input input-bordered w-full max-w-xs" />
+
+                <button className='btn btn-accent mt-2' type='submit'>Register</button>
+
+                <p className="text-white">Already have an account <span className="text-white"> <Link className='link link-primary' to="/login">Login</Link></span></p>
+
+            </form>
         </div>
     );
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,20 @@
 import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className='mb-10  container mx-auto bg-slate-400 rounded-lg py-10'>
+        <div className='mb-10  container mx-auto bg-cyan-950 rounded-lg py-10'>
             <h3 className=' w-80 py-2 mt-2 mx-auto text-purple-100 font-bold text-lg rounded-lg bg-purple-900 text-center'>Please Login</h3>
             <form className="form-control w-full max-w-xs mx-auto">
 
                 <label className="label">
-                    <span className="label-text font-bold">What is your Email ?</span>
+                    <span className="label-text font-bold text-white">What is your Email ?</span>
 
                 </label>
                 <input type="email" name='email' placeholder="Type here" required className="input input-bordered w-full max-w-xs" />
 
                 <label className="label">
-                    <span className="label-text font-bold">What is your Password?</span>
+                    <span className="label-text font-bold text-white">What is your Password?</span>
 
                 </label>
                 <input type="password" name='password' placeholder="Type here" required className="input input-bordered w-full max-w-xs" />
@@ -23,8 +24,8 @@ const Login = () => {
                 <div className='flex'>
                     <button className='btn btn-info w-1/2 mt-2' type='submit'> <span className='text-2xl me-2'> <FaGithub /> </span> Github</button>
                     <button className='btn btn-primary w-1/2 mt-2' type='submit'> <span className='text-red-400 me-2 text-2xl'><FaGoogle></FaGoogle></span>       Google</button>
-
                 </div>
+                <p className='text-white'>Do you new this website? please <span className='link active-link'><Link to='/registration'>registration</Link></span></p>
             </form>
 
         </div>
